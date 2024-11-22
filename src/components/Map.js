d8c97602e5aeadb8da97574cd64c76f9a0a5b63a
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2024-11-22 18:27:51 +0100

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,44 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+// Fix default icon issue with Leaflet
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 export default function Map() {
+  const [position, setPosition] = useState(null);
+
   useEffect(() => {
-    // Initialize the map using the external API
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          setPosition([pos.coords.latitude, pos.coords.longitude]);
+        },
+        (err) => {
+          console.error(err);
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
   }, []);
 
-  return <div id="map" style={{ height: '500px' }} />;
+  return (
+    <div id="map" style={{ height: '500px', width: '100%' }}>
+      {position && (
+        <MapContainer center={position} zoom={13} style={{ height: '100%', width: '100%' }}>
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <Marker position={position}>
+            <Popup>You are here</Popup>
+          </Marker>
+        </MapContainer>
+      )}
+    </div>
+  );
 }
