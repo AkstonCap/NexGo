2403393a5e0cab732e1a72c83b19307c917b822d
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2024-11-22 18:01:53 +0100

    Create Map.js

---
+++ src/components/Map.js
@@ -0,0 +1,9 @@
+import { useEffect } from 'react';
+
+export default function Map() {
+  useEffect(() => {
+    // Initialize the map using the external API
+  }, []);
+
+  return <div id="map" style={{ height: '500px' }} />;
+}
