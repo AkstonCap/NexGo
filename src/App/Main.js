519ae5965e621d1b4d88b43aa30b37273939ec0f
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2024-11-22 18:09:56 +0100

    Update Main.js

--- src/App/Main.js
+++ src/App/Main.js
@@ -27,46 +27,12 @@ const DemoTextField = styled(TextField)({
 export default function Main() {
   const coreInfo = useSelector((state) => state.nexus.coreInfo);
   const userStatus = useSelector((state) => state.nexus.userStatus);
-  const showingConnections = useSelector(
-    (state) => state.settings.showingConnections
-  );
   const inputValue = useSelector((state) => state.ui.inputValue);
   const dispatch = useDispatch();
-  const confirmToggle = async () => {
-    const question = showingConnections
-      ? 'Hide number of connections?'
-      : 'Show number of connections?';
-
-    const agreed = await confirm({ question });
-    if (agreed) {
-      if (showingConnections) {
-        dispatch(hideConnections());
-      } else {
-        dispatch(showConnections());
-      }
-    }
-  };
   const handleChange = (e) => {
     dispatch(updateInput(e.target.value));
   };
-  const [checkingMetrics, setCheckingMetrics] = useState(false);
-  const viewMetrics = async () => {
-    try {
-      setCheckingMetrics(true);
-      const result = await apiCall('system/get/metrics');
-      showSuccessDialog({
-        message: 'Tritium Metrics',
-        note: JSON.stringify(result, null, 2),
-      });
-    } catch (error) {
-      showErrorDialog({
-        message: 'Cannot get metrics',
-        note: error?.message || 'Unknown error',
-      });
-    } finally {
-      setCheckingMetrics(false);
-    }
-  };
+
 
   return (
     <Panel title="NexGo" icon={{ url: 'react.svg', id: 'icon' }}>
